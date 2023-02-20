037f4e5090dee5025f8b45f83c755c0e247b7149
Nicole Low <nicolexylow@gmail.com>
2023-02-21 10:48:03 +1100

    Hamburger menu

--- src/components/main/NavBar.jsx
+++ src/components/main/NavBar.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 export default function NavBar () {
+    const [isMobile, setIsMobile] = useState(true)
+
     return (
-        <nav className = 'nav'>
+        <div className = 'nav'>
             <h1>AfterLeaf</h1>
             <p>Plant Memorial &<br/>Reincarnation</p>
-            <ul>
-                <li>About us</li>
-                <li>Our newsletter</li>
-            </ul>
-            <a><img src="https://i.ibb.co/1924Z6L/cart.png" alt="cart" border="0" width = "25" height = "25"/></a>
-        </nav>
+
+            <nav>
+                <ul>
+                    <li className="nav-links">About us</li>
+                    <li className="nav-links">Our newsletter</li>
+                </ul>
+                <a className="nav-links"><img src="https://i.ibb.co/1924Z6L/cart.png" alt="cart" border="0" width = "25" height = "25"/></a>
+                
+                <button className="mobile-menu-icon">
+                    {isMobile ? <FaBars /> : <FaTimes />}
+                </button>
+            </nav>
+        </div>
     )
 }
