6cca9c0fbad47f6d2ca3d16117a553ee75df564b
Nicole Low <nicolexylow@gmail.com>
2023-02-21 13:45:43 +1100

    Dropdown hamburger

--- src/components/main/NavBar.jsx
+++ src/components/main/NavBar.jsx
@@ -1,25 +1,39 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 export default function NavBar () {
-    const [isMobile, setIsMobile] = useState(true)
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => {
+        setClick(!click)
+    }
 
     return (
         <div className = 'nav'>
-            <h1>AfterLeaf</h1>
-            <p>Plant Memorial &<br/>Reincarnation</p>
+
+            <div>
+                <h1>AfterLeaf</h1>
+                <p>Plant Memorial &<br/>Reincarnation</p>
+            </div>
+            
 
             <nav>
-                <ul>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li className="nav-links">About us</li>
                     <li className="nav-links">Our newsletter</li>
+
+                    {click ? <a className="nav-links"><img src="cart.png" alt="cart" border="0" width = "25" height = "25"/></a>
+                    : <a className="nav-links"><img src="https://i.ibb.co/1924Z6L/cart.png" alt="cart" border="0" width = "25" height = "25"/></a>
+                }
                 </ul>
-                <a className="nav-links"><img src="https://i.ibb.co/1924Z6L/cart.png" alt="cart" border="0" width = "25" height = "25"/></a>
-                
-                <button className="mobile-menu-icon">
-                    {isMobile ? <FaBars /> : <FaTimes />}
+
+                <button className="nav-icon" onClick={handleClick}>
+                    {click ? <FaTimes /> : <FaBars />}
                 </button>
             </nav>
+
+            
+            
         </div>
     )
 }
